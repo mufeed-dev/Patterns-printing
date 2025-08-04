7efd0d239a901c260d0d91c078f75ee12358b1e1
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 20:27:31 +0530

    F-Pattern

--- patterns.c
+++ patterns.c
@@ -906,7 +906,24 @@
 // }
 
 
+// E-pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n;j++){
+//             if(i==0||i==n-1||j==0||i==n/2) printf("* ");
+//             else printf("  ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
 
+
+// F-pattern
 #include<stdio.h>
 int main(){
     int n, l;
@@ -914,7 +931,7 @@ int main(){
     scanf("%d",&n);
     for(int i=0; i<n; i++){
         for(int j=0; j<n;j++){
-            if(i==0||i==n-1||j==0||i==n/2) printf("* ");
+            if(i==0||j==0||i==n/2) printf("* ");
             else printf("  ");
         }
         printf("\n");
