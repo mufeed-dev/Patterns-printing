914fce1032b1f85e01fc18b6bbba18716553c905
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 12:59:34 +0530

    down-arrow-pattern

--- patterns.c
+++ patterns.c
@@ -158,16 +158,35 @@
 
 
 // diamond star pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=-n; i<=n; i++){
+//         if(i<0) l = -i;
+//         else l = i;
+//         for(int j=0;j<2*n-l+1;j++){
+//             if(j<l)
+//                 printf("  ");
+//             else
+//                 printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// down-arrow pattern
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
-    for(int i=-n; i<=n; i++){
-        if(i<0) l = -i;
-        else l = i;
-        for(int j=0;j<2*n-l+1;j++){
-            if(j<l)
+    for(int i=0; i<=n; i++){
+        for(int j=-n;j<=n;j++){
+            if((-n+i)<j&&j<(n-i))
                 printf("  ");
             else
                 printf("* ");
