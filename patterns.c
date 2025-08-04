16b397eef7a80879ea17c8b1bc61f87dc09aa8ef
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 19:35:40 +0530

    Tiered Exponential Burst Pattern

--- patterns.c
+++ patterns.c
@@ -828,20 +828,38 @@
 
 
 //  Fractal Pulse Cascade
+// #include<stdio.h>
+// int main(){
+//     int n, l=0;
+//     printf("Enter the Number: ");
+//     scanf("%d",&n);
+//     for(int i =1; i<=n;i++){
+//         l++;
+//         if(i%2==1) printf("*\n");
+//         else printf("*\n*\n");
+//         if(l>3) l=1;
+//         if(i==n) break;
+//         for(int j=1;j<=((l==3)?10:4*l);j++)
+//             printf("* ");
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// Tiered Exponential Burst Pattern
 #include<stdio.h>
+#include<math.h>
 int main(){
     int n, l=0;
     printf("Enter the Number: ");
     scanf("%d",&n);
     for(int i =1; i<=n;i++){
-        l++;
-        if(i%2==1) printf("*\n");
-        else printf("*\n*\n");
-        if(l>3) l=1;
-        if(i==n) break;
-        for(int j=1;j<=((l==3)?10:4*l);j++)
+        for(int j=1;j<=pow(2,i);j++)
             printf("* ");
         printf("\n");
+        for(int j=1;j<=2*i-1;j++)
+            printf("*\n");
     }
     return 0;
 }
