6ed7b20ca31d5acde1f08e4c7a5c415fd4c77bab
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 12:38:45 +0530

    Add patterns.c

---
+++ patterns.c
@@ -0,0 +1,118 @@
+//square star pattern
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n;j++)
+//         printf("* ");
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// right Triangle
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<i+1;j++)
+//         printf("* ");
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// inverted half
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n-i;j++)
+//         printf("* ");
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// hollow square
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0||i==n-1||j==0||j==n-1)
+//             printf("* ");
+//             else printf("  ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// pyramid star
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n+i;j++){
+//             if(j<n-i-1)
+//                 printf("  ");
+//             else
+//                 printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// inverted pyramid
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<2*n-i-1;j++){
+//             if(j<i)
+//                 printf("  ");
+//             else
+//                 printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// mirror-image pattern
+#include<stdio.h>
+int main(){
+    int n, l;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(int i=-n; i<=n; i++){
+        if(i<0) l = -i;
+        else l = i;
+        for(int j=0;j<n+l+1;j++){
+            if(j<n-l) printf("  ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
