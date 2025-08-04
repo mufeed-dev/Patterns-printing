b4476e9697c71d1ef3cd788805c94138a9f2e915
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 22:41:08 +0530

    Tiered Step-Pulse Pattern

--- patterns.c
+++ patterns.c
@@ -1054,18 +1054,39 @@
 
 
 // S-pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0||i==n-1||i==n/2||((j==0)&&(i<=n/2))||((j==n-1)&&(i>=n/2)))
+//             printf("* ");
+//             else printf("  ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// Tiered Step-Pulse Pattern
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
-    for(int i=0; i<n; i++){
-        for(int j=0;j<n;j++){
-            if(i==0||i==n-1||i==n/2||((j==0)&&(i<=n/2))||((j==n-1)&&(i>=n/2)))
-            printf("* ");
-            else printf("  ");
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=i;j++){
+            for(int j=1;j<=2*i;j++)
+                printf("* ");
+            printf("\n");
         }
-        printf("\n");
+        if(n==i) break;
+        for(int j=1;j<4;j++)
+            printf("*\n");
     }
     return 0;
 }
