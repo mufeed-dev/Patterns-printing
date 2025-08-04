5844800481b671f97ae6dd7c603daad309dd1e14
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 13:25:48 +0530

    Right-Aligned Letter Triangle

--- patterns.c
+++ patterns.c
@@ -261,18 +261,33 @@
 // 3 4 5 1 2
 // 4 5 1 2 3
 // 5 1 2 3 4
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         l=1;
+//         for(int j=0;j<n;j++){
+//             if(i+j>=n)
+//                 printf("%d ",l++);
+//             else
+//                 printf("%d ",i+j+1);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Right-Aligned Letter Triangle
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
     for(int i=0; i<n; i++){
-        l=1;
-        for(int j=0;j<n;j++){
-            if(i+j>=n)
-                printf("%d ",l++);
-            else
-                printf("%d ",i+j+1);
+        for(int j=0;j<i+1;j++){
+            printf("%c ",'A'+i);
         }
         printf("\n");
     }
