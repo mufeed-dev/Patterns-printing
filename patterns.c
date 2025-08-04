79815cf7768f5bee4181d162a013758d65134690
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 13:05:11 +0530

    invmirror-righTriangle-pattern

--- patterns.c
+++ patterns.c
@@ -199,14 +199,34 @@
 
 
 // up-arrow pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<=n; i++){
+//         for(int j=-n;j<=n;j++){
+//             if(-i<j&&j<i)
+//                 printf("  ");
+//             else
+//                 printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// inverted-mirror-right-triangle
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
-    for(int i=0; i<=n; i++){
-        for(int j=-n;j<=n;j++){
-            if(-i<j&&j<i)
+    for(int i=0; i<n; i++){
+        for(int j=0;j<n;j++){
+            if(j<i)
                 printf("  ");
             else
                 printf("* ");
