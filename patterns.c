25696871bb01cf4a4e0576945ecb06133689b13c
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 22:15:42 +0530

    Double-Aligned Diamond Pattern

--- patterns.c
+++ patterns.c
@@ -981,20 +981,35 @@
 // }
 
 // Asymmetric Diamond Pulse Pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l=1;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=2*n+1-2*i;j++)
+//             printf("* ");
+//         printf("\n");
+//         for(int j=1; j<=i;j++)
+//             printf("* ");
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// Double-Aligned Diamond Pattern
 #include<stdio.h>
 int main(){
-    int n, l=1;
+    int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
-    for(int i=1; i<=n; i++){
-        for(int j=1;j<=2*n+1-2*i;j++)
-            printf("* ");
-        printf("\n");
-        for(int j=1; j<=i;j++)
+    for(int i=-n; i<=n; i++){
+        l = (i<0) ? -i: i;
+        for(int j=0;j<n-l+1;j++)
             printf("* ");
         printf("\n");
     }
     return 0;
 }
-
 
