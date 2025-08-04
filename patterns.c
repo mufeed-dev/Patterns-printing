1624d3f2fced8ddafcf480596abf61ae2dc99151
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-04 20:25:54 +0530

    E-Pattern

--- patterns.c
+++ patterns.c
@@ -888,23 +888,39 @@
 // }
 
 
+// Mirrored Right Triangle Pattern
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=-n; i<=n; i++){
+//         l =(i<0) ? -i : i;
+//         for(int j=0; j<2*(n-l)-1;j++){
+//             if(j<n-l-1) printf("  ");
+//             else printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
 
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
-    for(int i=-n; i<=n; i++){
-        l =(i<0) ? -i : i;
-        for(int j=0; j<2*(n-l)-1;j++){
-            if(j<n-l-1) printf("  ");
-            else printf("* ");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n;j++){
+            if(i==0||i==n-1||j==0||i==n/2) printf("* ");
+            else printf("  ");
         }
         printf("\n");
     }
     return 0;
 }
-
 
 
 
