f06a04230b3c2f58e36e6cfdfc5993a658612f7a
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-05 12:21:16 +0530

    Right-Aligned Odd Number Triangle

--- patterns.c
+++ patterns.c
@@ -1111,18 +1111,35 @@
 
 
 // hollow square with 'X'
+// #include<stdio.h>
+// int main(){
+//     int n, l;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=0; i<n; i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0||i==n-1||j==0||j==n-1||i==j||n==i+j+1) 
+//                 printf("* ");
+//             else printf("  ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// Right-Aligned Odd Number Triangle
 #include<stdio.h>
 int main(){
     int n, l;
     printf("Enter the number: ");
     scanf("%d",&n);
     for(int i=0; i<n; i++){
-        for(int j=0;j<n;j++){
-            if(i==0||i==n-1||j==0||j==n-1||i==j||n==i+j+1) 
-                printf("* ");
-            else printf("  ");
-        }
+       for(int j=0; j<n-i;j++)
+        printf("%d ",1+2*j);
         printf("\n");
     }
     return 0;
 }
+
